c6258c28554a56076bcfd04f9cb23bb27fb8bfcb
dannyLiu <h152074716@gmail.com>
2018-02-09 21:23:56 +0800

    初次爬蟲

--- select.js
+++ select.js
@@ -1,18 +1,19 @@
-var http = require('http');
+var https = require('https');
 var fs = require('fs');
 var cheerio = require('cheerio');
 
-var URL = "http...";
+var URL = "https://zh.wikipedia.org/wiki/%E5%8F%B0%E7%81%A3%E5%AA%BD%E7%A5%96%E4%BF%A1%E4%BB%B0";
 
-http.get(URL,function(res){
+https.get(URL,function(res){
     console.log("http stat code ="+res.statusCode);
     res.on('data',function(chunk){
-        console.log(chunk.toString());
+        //console.log(chunk.toString());
         var $ = cheerio.load(chunk);
         var arr =[];
-        var goal = $("table tb tr");
-        goal.each();            
-        });
+        var goal = $(".wikitable tr td");
+        goal.each(function(i,ele){
+            console.log($(this).text());
+        });            
     });
 }).on('error',function(e){
     console.log("error:"+e.message);
