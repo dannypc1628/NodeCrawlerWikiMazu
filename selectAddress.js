6449afa2fb1aeb0030b0560d04e36065bbb6c2ab
dannyLiu <h152074716@gmail.com>
2018-02-12 12:19:40 +0800

    批次取得

--- selectAddress.js
+++ selectAddress.js
@@ -1,38 +1,36 @@
 
 var fs = require("fs");
+
 var googleMapsClient = require("@google/maps").createClient({
-    key: "a"
+    key: k
 });
 
-googleMapsClient.places({
-    query: "彰化縣 埤頭鄉 埤頭合興宮"
-    }, function(err, response) {
-     if (!err) {
-       //console.log(response.json.results);
-       var data = response.json.results;
-       var obj = JSON.stringify(data);
-      
-      }
-    });
 
-// var fs = require('fs');`
-// var obj;
-//   fs.readFile('data.json', 'utf8', function (err, data) {
-//     if (err) throw err;
-//     obj = JSON.parse(data);
-//     console.log(obj.table[0].county);
-//     console.log(obj.table.length);
-
-//     for(var i = 0;i < obj.table.length;i++){
-//         googleMapsClient.places({
-//             query: "彰化縣 埤頭鄉 埤頭合興宮"
-//             }, function(err, response) {
-//              if (!err) {
-//                console.log(response.json.results);
-//               }
-//             });
-//     }
-//   });
+var fs = require('fs');
+var obj;
+  fs.readFile('data.json', 'utf8', function (err, data) {
+    if (err) throw err;
+    obj = JSON.parse(data);
+    console.log(obj.table[0].county);
+    console.log(obj.table.length);
+    var objStr = obj.table;
+    //obj.table.length
+    for(var i = 4;i < 6 ;i++){
+        googleMapsClient.places({
+            query: objStr[i].county+" "+objStr[i].location+" "+objStr[i].name,
+            language:"zh-TW"
+            }, function(err, response) {
+                //console.log(objStr[i].county+" "+objStr[i].location+" "+objStr[i].name);
+             if (!err) {
+                
+                var results = response.json.results;
+                console.log(results[0].name);
+                console.log(results[0].formatted_address);
+                console.log(results[0].geometry.location);
+              }
+            });
+    }
+  });
 
 
 
